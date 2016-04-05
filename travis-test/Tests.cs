c4ccc01eb08b5bdbd73f22d44ab6c29e2bbf95ce
Josh Schreuder <jschreuder@bcc.com.au>
2016-04-05 16:04:56 +1000

    Test Travis-CI with ApprovalTests

---
+++ travis-test/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using ApprovalTests;
+using Xunit;
+namespace travis_test
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestNormal()
+        {
+            Assert.Equal(4, 2 * 2);
+        }
+
+        [Fact]
+        public void TestApprovals()
+        {
+            Approvals.VerifyAll(new Dictionary<int, int>
+            {
+                { 2, 1 * 2 },
+                { 4, 2 * 2 },
+                { 6, 3 * 2 },
+                { 8, 4 * 2 }
+            });
+        }
+    }
+}
